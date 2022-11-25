2f9479442b2d1ab1b25bc241b4ad676e0a24015b
latroner <antoshka040703@mail.ru>
2022-11-25 19:13:48 +0300

    Новый пустой и бесполезный файл

--- rehex.py
+++ rehex.py
@@ -7,4 +7,13 @@ Original file is located at
     https://colab.research.google.com/drive/1YbDcdwLTpA_mFx29FVoptZCNT0zhRR-u
 """
 
-https?://(?:\w)(?:.[a-z]+)+(?:/[a-zA-Z0-9.?=&%;-_])*
+import re 
+patfh ="/content/Прикл. прогр. (090303++)_ Лабораторная работа № 3 «Регулярные выражения. Работа с Git».html"#input("введите путь: ")
+
+with open(patfh) as f:
+  match = re.findall('https?:\/\/(?:\w*)(?:\.[a-z]+)+(?:\/[a-zA-Z0-9.?=&%;\-_]*)*[^,\'.?"}\]]', f.read() ) 
+  if match:
+    for ssil in match:
+      print(ssil)
+  else:
+    print("ссылок нет")
